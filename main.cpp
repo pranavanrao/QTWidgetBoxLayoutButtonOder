80b5f3415e1bfefbf813be3e9bbd6951fd62ebf9
Pranav N Rao <pranavanrao@gmail.com>
2024-04-16 11:41:01 +0530

    Add vertical and horizontal layouts

--- main.cpp
+++ main.cpp
@@ -3,26 +3,42 @@
 #include <QApplication>
 #include <QPushButton>
 #include <QHBoxLayout>
+#include <QVBoxLayout>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
+    QWidget  w;
 
     QPushButton b1("Button1");
     QPushButton b2("Button2");
     QPushButton b3("Button3");
     QPushButton b4("Button4");
 
-    // QWidget widget;
+    QPushButton b11("Button1");
+    QPushButton b12("Button2");
+    QPushButton b13("Button3");
+    QPushButton b14("Button4");
+
     QHBoxLayout *hLayout = new QHBoxLayout;
+    QVBoxLayout *vLayout = new QVBoxLayout;
 
     hLayout -> addWidget(&b1);
     hLayout -> addWidget(&b2);
     hLayout -> addWidget(&b3);
     hLayout -> addWidget(&b4);
 
-    w.setLayout(hLayout);
+    vLayout -> addWidget(&b11);
+    vLayout -> addWidget(&b12);
+    vLayout -> addWidget(&b13);
+    vLayout -> addWidget(&b14);
+
+    QVBoxLayout *vMainLayout = new QVBoxLayout;
+
+    vMainLayout -> addLayout(hLayout);
+    vMainLayout -> addLayout(vLayout);
+
+    w.setLayout(vMainLayout);
     w.show();
 
     return a.exec();
